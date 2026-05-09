f1da5499e5e2fcc8fce9f9e1f0742d5f85131f2c
ayushk-sinha <sinha.ayush2406@gmail.com>
2026-05-09 11:31:22 +0530

    fix

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import router from './router'
 
 const app = createApp(App)
 //const head = createHead()
-app.use(head)
+//app.use(head)
 // app.use(createPinia())
 app.use(router)
 app.mount('#app')
--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,6 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-export const SITE_URL = 'https://repwar.live'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
